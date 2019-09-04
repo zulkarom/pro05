b1aa34b0437f4c57c359d18b449961791b5e1c92
zulkarom <zulkarom@gmail.com>
2019-09-04 12:54:08 +0800

    project module

---
+++ backend/modules/project/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\project;
+
+/**
+ * project module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\project\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ frontend/modules/project/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\project;
+
+/**
+ * project module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\project\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
