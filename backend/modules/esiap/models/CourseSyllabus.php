0d3f9017af41f263a2c95738b5286e74da3d84f6
zulkarom <zulkarom@gmail.com>
2020-11-18 14:46:14 +0800

    fix clone

--- backend/modules/esiap/models/CourseSyllabus.php
+++ backend/modules/esiap/models/CourseSyllabus.php
@@ -41,13 +41,13 @@ class CourseSyllabus extends \yii\db\ActiveRecord
 			
 			[['crs_version_id'], 'required', 'on' => 'addweek'],
 			
-			[['pnp_lecture', 'pnp_tutorial', 'pnp_practical', 'pnp_others', 'independent',  'nf2f'], 'required', 'on' => 'slt'],
+			[['pnp_lecture', 'pnp_tutorial', 'pnp_practical', 'pnp_others', 'tech_lecture', 'tech_tutorial', 'tech_practical', 'tech_others', 'independent',  'nf2f'], 'required', 'on' => 'slt'],
 			
             [['crs_version_id', 'syl_order', 'duration'], 'integer'],
 			
             [['topics', 'week_num'], 'string'],
 			
-            [['pnp_lecture', 'pnp_tutorial', 'pnp_practical', 'pnp_others', 'independent', 'assessment', 'nf2f'], 'number'],
+            [['pnp_lecture', 'pnp_tutorial', 'pnp_practical', 'pnp_others', 'tech_lecture', 'tech_tutorial', 'tech_practical', 'tech_others', 'independent', 'assessment', 'nf2f'], 'number'],
 			
             [['clo'], 'string', 'max' => 255],
         ];
