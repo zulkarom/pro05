7e6ecf77ee778e1c1c32a8e5671ed94f820852a2
zulkarom <zulkarom@gmail.com>
2021-04-12 16:23:45 +0800

    frontend

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -16,6 +16,7 @@ use frontend\models\SignupForm;
 use frontend\models\ContactForm;
 use frontend\models\CourseSearch;
 use yii\web\ForbiddenHttpException;
+use TCPDF;
 
 /**
  * Site controller
@@ -216,4 +217,18 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
+	
+	public function actionTestPdf(){
+		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+		$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+		$pdf->AddPage();
+$html = <<<EOD
+<h1>Berjaya!</h1>
+EOD;
+		$pdf->writeHTMLCell($w=0, $h=0, $x='', $y='', $html, $border=0, $ln=1, $fill=0, $reseth=true, $align='', $autopadding=true);
+		$pdf->Output('example_001.pdf', 'I');
+		exit();
+	}
 }
